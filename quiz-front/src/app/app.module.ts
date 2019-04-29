2b750bde3baba258ac953a59968baf646f00899e
asselyer <asel.yer98@gmail.com>
2019-04-30 01:12:43 +0600

    Quiz 1 Assel Yernazarova

---
+++ quiz-front/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProviderService } from './provider.service';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    ProviderService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
