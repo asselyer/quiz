d51f5544d291769daf298245b8fd4ae3f7592ed2
asselyer <asel.yer98@gmail.com>
2019-04-30 01:42:43 +0600

    Quiz 1 Assel Yernazarova

--- quiz-front/src/app/app.module.ts
+++ quiz-front/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ClassProvider, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProviderService } from './provider.service';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AuthInterceptor} from './AuthInterceptor';
 
 
 @NgModule({
@@ -18,6 +19,11 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   providers: [
     ProviderService,
+    <ClassProvider> {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
